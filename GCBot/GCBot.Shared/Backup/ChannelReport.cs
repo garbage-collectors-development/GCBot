4b4d24a907474607329634896c37f23c8adb918e
Julian <julian.feenstra0@gmail.com>
2019-11-15 00:34:22 +0100

    ignored dtos

---
+++ GCBot/GCBot.Shared/Backup/ChannelReport.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace GCBot.Shared.Backup
+{
+    public class ChannelReport : Report<uint, UserReport>
+    {
+        public ChannelReport(uint channelId, DateRange dateRange) : base(channelId, dateRange)
+        { }
+
+        public override int TotalMessages =>
+            Information.Values.Sum(a => a.TotalMessages);
+    }
+}
---
+++ GCBot/GCBot.Shared/Backup/UserReport.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace GCBot.Shared.Backup
+{
+    public class UserReport : Report<DateTime, int>
+    {
+        public UserReport(uint userId, DateRange dateRange) : base(userId, dateRange) { }
+
+        public override int TotalMessages =>
+            Information.Values.Sum(a => a);
+    }
+}
