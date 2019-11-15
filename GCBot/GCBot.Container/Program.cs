9f448690e5a268c49087403cb2e0bf8476c60122
Jeremy Guinn <jrguinn@mtu.edu>
2019-11-14 22:57:47 -0500

    Add missing DI for BackupContext

--- GCBot/GCBot.Container/Program.cs
+++ GCBot/GCBot.Container/Program.cs
@@ -16,7 +16,8 @@ namespace GCBot.Container
             var serviceCollection = new ServiceCollection();
 
             serviceCollection.AddSingleton<IBackupService, BackupService>()
-                .AddSingleton<IBackupRepository, BackupRepository>();
+                .AddSingleton<IBackupRepository, BackupRepository>()
+                .AddSingleton(new BackupContext(""));
 
             Client client = new Client(serviceCollection);
             await client.RunAsync();
